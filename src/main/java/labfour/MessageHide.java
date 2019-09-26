be114865672139ae4d57e205684e63c5baaf8a6b
jjumadin <jjumadin@aldenv64-6.local>
2019-09-25 21:28:46 -0400

    Starter materials for lab 4

---
+++ src/main/java/labfour/MessageHide.java
@@ -0,0 +1,38 @@
+package labfour;
+
+/** This class has a method that implements steganography method.
+*
+* <p>Bugs: This program has no known bugs
+*
+* @author Add Your Names Here
+*/
+
+public class MessageHide {
+
+  /** The main entry point for MessageHide.
+  *  The source code inside of this method will be executed when the program is
+  *  run in the command-line. The program will hide user's input inside
+  *  a grid of 20 by 20.
+  *
+  **/
+  public static void main(String[] args) {
+    // display the name of the programmer and the date
+    System.out.println("Add Your Names Here " + new Date());
+
+    // TODO: declare the scanner
+
+    // TODO: ask the user to enter the message to be hidden
+
+    // TODO: Read in and display the chosen message
+
+    // TODO: convert to upper case
+
+    // TODO: Assemble the message by using at least 5 new String variables
+
+    // TODO: Use println statements to display the 20 x 20 grid
+
+    // TODO: Display a final thank you message
+    
+  }
+
+}
---
+++ writing/reflection.md
@@ -0,0 +1,28 @@
+# Reflection by Janyl Jumadinova
+
+## Using a fenced code block, please display the output from running your program
+
+```
+
+```
+
+## For each String method used, add an explanation in the list format (as shown in an example below).
+
+- `toUpperCase`: convert characters to uppercase
+
+## What is your message hiding strategy?
+
+
+## What was the greatest technical challenge that your team faced and how did you overcome it?
+
+
+## After completing this assignment, what is task that your team wants to practice more? Why?
+
+
+## After completing this assignment, what is one experience for which your team is grateful?
+
+
+## What was your team-working strategy?
+
+
+## Write a reflection on computational thinking following guided questions in the assignment sheet.
